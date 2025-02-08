571b2054f9c04b35dc3901c39079b023b24c684c
sofiatsu <rfjdghjgh@gmail.com>
2025-02-08 13:10:10 +0100

    ДЗ 5.1.

--- lesson5.1.py
+++ lesson5.1.py
@@ -4,15 +4,11 @@ import keyword
 name = str(input('Enter variable name: '))
 allowed = string.ascii_lowercase + string.digits + '_'
 
-if name not in allowed:
+if name[0].isdigit():
     print('False')
-elif name[0].isdigit():
+elif not all(c in allowed for c in name):
     print('False')
 elif name in keyword.kwlist:
-    print('False')
-elif name.count('_') > 1:
-    print('False')
-elif name.isupper():
     print('False')
 else:
     print('True')
